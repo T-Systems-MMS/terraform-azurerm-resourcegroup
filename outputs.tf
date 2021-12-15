f9436d9e75489b03a7d2b37d3e8d164ac8ece985
mima <Michaela.Mattes@t-systems.com>
2021-12-15 22:10:50 +0100

    fix: fmt

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "resource_group" {
   description = "azurerm_resource_group results"
   value = {
-    for resource_group in  keys(azurerm_resource_group.resource_group):
+    for resource_group in keys(azurerm_resource_group.resource_group) :
     resource_group => {
       name = azurerm_resource_group.resource_group[resource_group].name
       id   = azurerm_resource_group.resource_group[resource_group].id
